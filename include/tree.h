a7d31299d9f3e42660b1493ceb80246c8bcf8250
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-26 16:23:50 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -2,4 +2,63 @@
 #ifndef INCLUDE_TREE_H_
 #define INCLUDE_TREE_H_
 
+#include <vector>
+#include <algorithm>
+#include <stack>
+
+class Tree {
+ private:
+    struct Node {
+        char data;
+        std::vector<Node*> children;
+    };
+    Node* root;
+    std::vector<std::vector<char>> permutations;
+    void generatePermutations(Node* node, std::vector<char> currentPerm) {
+        currentPerm.push_back(node->data);
+        if (node->children.empty()) {
+            permutations.push_back(currentPerm);
+            return;
+        }
+        for (auto child : node->children) {
+            generatePermutations(child, currentPerm);
+        }
+    }
+
+ public:
+    Tree(std::vector<char>& elements) {
+        root = new Node{' ', {}};
+        std::stack<Node*> nodes;
+        nodes.push(root);
+        for (auto i : elements) {
+            while (!nodes.top()->children.empty() && nodes.top()->data != i) {
+                nodes.pop();
+            }
+            Node* newNode = new Node{i, {}};
+            nodes.top()->children.push_back(newNode);
+            nodes.push(newNode);
+        }
+        std::vector<char> currentPerm;
+        generatePermutations(root, currentPerm);
+    }
+    ~Tree() {
+        deleteSubtree(root);
+    }
+    std::vector<char> getPerm(int n) {
+        if (n < 1 || n > permutations.size()) {
+            return std::vector<char>{};
+        }
+
+        return permutations[n - 1];
+    }
+
+ private:
+    void deleteSubtree(Node* node) {
+        for (auto child : node->children) {
+            deleteSubtree(child);
+        }
+        delete node;
+    }
+};
+
 #endif  // INCLUDE_TREE_H_
