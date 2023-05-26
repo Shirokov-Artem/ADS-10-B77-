0cbc7f62460ed3162755bd322dadb52082204c2d
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-27 02:27:51 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -3,10 +3,25 @@
 #include  <fstream>
 #include  <locale>
 #include  <cstdlib>
-#include <vector>
-#include <algorithm>
 #include  "tree.h"
 
+int fact(int n) {
+    if (n <= 1) return 1;
+    return fact(n-1) * n;
+}
+
 std::vector<char> getPerm(const Tree& tree, int n) {
-    return tree.permutations[n - 1];
+    std::vector<char> result;
+    getPerm(tree, n, result);
+    return result;
+}
+
+void getPerm(const Tree& tree, int n, std::vector<char>& result) {
+    if (tree.getSize() == 0 || n > fact(tree.getSize())) return;
+
+    if (tree.getSymb() == '*') n--;
+
+    int temp = fact(tree.getSize()-1);
+    result.push_back(tree.getChild(n/temp)->symb);
+    getPerm(Tree(tree.getChild(n/temp)->children), n%temp, result);
 }
