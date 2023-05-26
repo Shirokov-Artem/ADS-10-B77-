ad7f319bb32914bd1a16e0349e14476ccd931c83
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-27 01:01:27 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -8,10 +8,5 @@
 #include  "tree.h"
 
 std::vector<char> getPerm(const Tree& tree, int n) {
-    std::vector<std::vector<char>> permutations;
-    tree.generate_permutations(tree.get_root(), {}, permutations);
-    if (n > static_cast<int>(permutations.size())) {
-        return {};
-    }
-    return permutations[n - 1];
+    return tree.getPermutation(n);
 }
