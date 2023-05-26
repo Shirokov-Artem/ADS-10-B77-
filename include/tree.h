b0a4a04d17dd0026312706c9a66c62408c450833
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-27 00:49:59 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -7,56 +7,53 @@
 
 class Tree {
     struct Node {
-        char value = 0;
+        char value;
         std::vector<Node*> children;
+        Node(char value) : value(value) {}
+        ~Node() {
+            for (auto child : children) {
+                delete child;
+            }
+        }
     };
     Node* root = nullptr;
 
  public:
-    Tree(std::vector<char> values) {
-        root = new Node;
+    explicit Tree(std::vector<char> values) {
+        root = new Node(' ');
         for (auto value : values) {
-            Node* child_node = new Node;
-            child_node->value = value;
-            root->children.push_back(child_node);
+            root->children.push_back(new Node(value));
         }
-        build_tree(values, root->children);
-    }
-    ~Tree() {
-        delete_node(root);
+        build_tree(root->children, values);
     }
     Node* get_root() const {
         return root;
     }
-    void generate_permutations(Node* node, std::vector<char> current_perm, 
+    void generate_permutations(const Node* node, 
+std::vector<char> current_perm, 
 std::vector<std::vector<char>>& permutations) const {
         current_perm.push_back(node->value);
         if (node->children.empty()) {
             permutations.push_back(current_perm);
         }
         for (auto child : node->children) {
-            generate_permutations(child, current_perm, permutations);
+            generate_permutations(child, current_perm, 
+permutations);
         }
     }
 
  private:
-    void delete_node(Node* node) {
-        for (auto child : node->children) {
-            delete_node(child);
-        }
-        delete node;
-    }
-    void build_tree(std::vector<char> remaining, std::vector<Node*>& current_nodes) {
+    void build_tree(std::vector<Node*>& current_nodes, 
+std::vector<char> remaining) {
         for (auto node : current_nodes) {
-            auto iter = std::find(remaining.begin(), remaining.end(), node->value);
+            auto iter = std::find(remaining.begin(), remaining.end(), 
+node->value);
             if (iter != remaining.end()) {
                 remaining.erase(iter);
                 for (auto value : remaining) {
-                    Node* child_node = new Node;
-                    child_node->value = value;
-                    node->children.push_back(child_node);
+                    node->children.push_back(new Node(value));
                 }
-                build_tree(remaining, node->children);
+                build_tree(node->children, remaining);
                 remaining.insert(iter, node->value);
             }
         }
