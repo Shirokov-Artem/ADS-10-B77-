da12ae3097b79ba62e0c1518603c66a55373f40f
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-27 03:06:22 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,22 +6,34 @@
 #include  "tree.h"
 
 int fact(int n) {
-    if (n <= 1) return 1;
-    return fact(n-1) * n;
-}
-
-std::vector<char> getPerm(const Tree& tree, int n) {
-    std::vector<char> result;
-    getPerm(tree, n, result);
+    int result = 1;
+    for (int i = 2; i <= n; i++) {
+        result *= i;
+    }
     return result;
 }
 
-void getPerm(const Tree& tree, int n, std::vector<char>& result) {
-    if (tree.getSize() == 0 || n > fact(tree.getSize())) return;
+std::vector<char> getPerm(const Tree& tree, int n) {
+    if (n <= 0 || n > fact(tree.getSize()))
+        return {};
 
-    if (tree.getSymb() == '*') n--;
+    std::vector<char> data = tree.getPerm();
+    std::vector<int> perm(tree.getSize());
+    for (int i = 0; i < tree.getSize(); i++) {
+        perm[i] = i;
+    }
 
-    int temp = fact(tree.getSize()-1);
-    result.push_back(tree.getChild(n/temp)->symb);
-    getPerm(Tree(tree.getChild(n/temp)->children), n%temp, result);
+    std::vector<char> result(tree.getSize());
+    int div = fact(tree.getSize() - 1);
+    for (int i = 0; i < tree.getSize(); i++) {
+        int index = n / div;
+        result[i] = data[perm[index]];
+        perm.erase(perm.begin() + index);
+        n = n % div;
+        if (i < tree.getSize() - 1) {
+            div /= (tree.getSize() - 1 - i);
+        }
+    }
+
+    return result;
 }
