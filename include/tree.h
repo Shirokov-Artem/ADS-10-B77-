00e0990fd4e1fa2ca26fd4caa5a6f231a22dcd77
Артем Широков <125153836+Shirokov-Artem@users.noreply.github.com>
2023-05-27 17:47:42 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -7,63 +7,65 @@
 
 class Tree {
  private:
-    struct Node {
-        char value;
-        std::vector<Node*> children;
-        Node(char val) : value(val) {}
-        ~Node() {
-            for (auto child : children) {
-                delete child;
-            }
-        }
-    };
+  struct Node {
+    char value;
+    std::vector<Node*> children;
 
-    std::vector<char> elements;
-    std::vector<std::vector<char>> permutations;
-    Node* root;
+    explicit Node(char val) : value(val) {}
 
-    void fillNode(Node* node, std::vector<char> values) {
-        values.erase(std::remove(values.begin(), values.end(),
-                                 node->value), values.end());
-        for (auto value : values) {
-            node->children.push_back(new Node(value));
-        }
-        for (auto child : node->children) {
-            fillNode(child, values);
-        }
+    ~Node() {
+      for (auto child : children) {
+        delete child;
+      }
     }
+  };
 
-    void generatePermutations(Node* node, std::vector<char> permutation) {
-        if (node->children.empty()) {
-            permutation.push_back(node->value);
-            permutations.push_back(permutation);
-            permutation.pop_back();
-        } else {
-            permutation.push_back(node->value);
-            for (auto child : node->children) {
-                generatePermutations(child, permutation);
-            }
-            permutation.pop_back();
-        }
+  std::vector<char> elements;
+  std::vector<std::vector<char>> permutations;
+  Node* root;
+
+  void fillNode(Node* node, std::vector<char> values) {
+    values.erase(std::remove(values.begin(), values.end(),
+                             node->value), values.end());
+    for (auto value : values) {
+      node->children.push_back(new Node(value));
     }
+    for (auto child : node->children) {
+      fillNode(child, values);
+    }
+  }
+
+  void generatePermutations(Node* node, std::vector<char> permutation) {
+    if (node->children.empty()) {
+      permutation.push_back(node->value);
+      permutations.push_back(permutation);
+      permutation.pop_back();
+    } else {
+      permutation.push_back(node->value);
+      for (auto child : node->children) {
+        generatePermutations(child, permutation);
+      }
+      permutation.pop_back();
+    }
+  }
 
  public:
-    Tree(const std::vector<char>& input) : elements(input),
+  explicit Tree(const std::vector<char>& input) : elements(input),
  root(new Node(' ')) {
-        for (auto value : elements) {
-            root->children.push_back(new Node(value));
-        }
-        for (auto child : root->children) {
-            fillNode(child, elements);
-            generatePermutations(child, std::vector<char>());
-        }
+    for (auto value : elements) {
+      root->children.push_back(new Node(value));
     }
-
-    ~Tree() {
-        delete root;
+    for (auto child : root->children) {
+      fillNode(child, elements);
+      generatePermutations(child, std::vector<char>());
     }
+  }
 
-    friend std::vector<char> getPerm(const Tree& tree, int n);
+  ~Tree() {
+    delete root;
+  }
+
+  friend std::vector<char> getPerm(const Tree& tree, int n);
 };
 
 #endif  // INCLUDE_TREE_H_
